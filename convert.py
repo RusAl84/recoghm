9d0fd7bbb7e6726982624c37bd88f74c54dc28be
Aleksey <rusal@bk.ru>
2023-06-15 23:15:50 +0300

    add new process

--- convert.py
+++ convert.py
@@ -1,22 +1,57 @@
 ﻿import os
 import base64
+from PIL import Image
+import numpy as np
+
+im_size =(200,100)
 
 def convert(path):
     for person in os.listdir(path):
         person = os.path.join(f"{path}/{person}")
         # print(person)
         for filename in os.listdir(person):
-            filename = os.path.join(f"{person}/{filename}")
-            # print(filename)
-            with open(filename, encoding="utf8") as file:
-                text = str(file.readlines())
-                img_data = text[24:-1]
-                with open(f"{filename}.png", "wb") as fh:
-                    print(img_data)
-                    fh.write(base64.b64decode(img_data))
-        
+            if filename.endswith(".txt"):
+                filename = os.path.join(f"{person}/{filename}")
+                # print(filename)
+                with open(filename, 'rb') as file:
+                    text = str(file.read())
+                    img_data = text[24:-1]
+                    file_path=f"{filename}.png"
+                    if  not os.path.exists(file_path):
+                        with open(file_path, "wb") as fh:
+                            img_data=str(img_data).encode("UTF-8")
+                            de =base64.decodebytes(bytes(img_data))
+                            fh.write(de)
+                            
+
+def getPixels(filename):    
+    im = Image.open(filename, 'r').convert('L')
+    im = im.resize(im_size)
+    # pix_val = list(im.getdata())
+    pix_val = np.asarray(im)
+    return pix_val
+    
+def process(path):
+    for person in os.listdir(path):
+        pp = os.path.join(f"{path}/{person}")
+        # print(person)
+        all_px=np.empty([im_size[1],im_size[0]])
+        for filename in os.listdir(pp):
+            if filename.endswith(".png"):
+                filename = os.path.join(f"{path}/{person}/{filename}")
+                px = getPixels(filename)
+                for i in range(im_size[1]):
+                    for j in range(im_size[0]):
+                        all_px[i,j]+=px[i,j]
+
+                # print(px)
+        PIL_image = Image.fromarray(np.uint8(all_px)).convert('L')
+        PIL_image.save(os.path.join(f"{path}/{person}/all_px.bmp"))
+        print(all_px)
         
 if __name__ == "__main__":
     path = os.path.join("./img")
-
     convert(path)
+    process(path)
+    
+    
